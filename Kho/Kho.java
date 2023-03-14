3a02330c1125beb9c28ee9dc27059dfd264761da
lenguyenthehien2205 <lenguyenthehien2205@gmail.com>
2023-03-14 23:25:27 +0700

    Huy gay

---
+++ Kho/Kho.java
@@ -0,0 +1,43 @@
+package Kho;
+
+public class Kho {
+    private String maKho;
+    private String tenKho;
+    private String diaChi;
+
+    public Kho() {
+        maKho = "";
+        tenKho = "";
+        diaChi = "";
+    }
+
+    public Kho(String maKho, String tenKho, String diaChi) {
+        this.maKho = maKho;
+        this.tenKho = tenKho;
+        this.diaChi = diaChi;
+    }
+
+    public String getMaKho() {
+        return maKho;
+    }
+
+    public void setMaKho(String maKho) {
+        this.maKho = maKho;
+    }
+
+    public String getTenKho() {
+        return tenKho;
+    }
+
+    public void setTenKho(String tenKho) {
+        this.tenKho = tenKho;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }  
+}
