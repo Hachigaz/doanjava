e76a8cac893fe441646ae50a9bf1d3ebd2ea349f
lamhongphong512 <lmhngphong@gmail.com>
2023-03-22 07:36:38 +0700

    s

--- Program.java
+++ Program.java
@@ -5,13 +5,15 @@ import SQL.SQLHandler;
 
 public class Program {
     private final String url = "jdbc:mysql://localhost:3306/QuanLyKho";
+    private final String url2 = "jdbc:mysql://192.168.25.159:3306/QuanLyKho";
+
     private final String username = "master";
     private final String password = "123";
 
     private SQLHandler masterHandler;
 
     public Program(){
-        masterHandler = new SQLHandler(url, username, password);
+        masterHandler = new SQLHandler(url2, username, password);
 
         DangNhapUI dangNhapUI = new DangNhapUI();
         ActionListener submitAction = new ActionListener() {
