7376491663e8ea15af26b5372ba7f60e85c71c91
lenguyenthehien2205 <lenguyenthehien2205@gmail.com>
2023-03-27 02:06:55 +0700

    Huy gay

--- Panel/Test.java
+++ Panel/Test.java
@@ -1,65 +1,62 @@
 package Panel;
 
-import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.border.Border;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.concurrent.Flow;
 import java.awt.*;
 
 public class Test extends JFrame implements MouseListener{
     JPanel panelLeft,panelRight,panelTop,panelIcon,panelUser;
     public String[] str = {"Danh sách sản phẩm","Danh mục sản phẩm","Khách hàng","Nhà cung cấp","Nhập/Xuất kho"};
+    public String[] img = {"danhMuc.png","danhSach.png","users.png","nhaCungCap.png","kho.png"};
     JLabel[] labels = new JLabel[5];
-    JLabel label1,labelIcon1,labelIcon2,labelUserName;
+    JLabel label1,labelIcon1,labelIcon2,labelUserName,labelTitle;
     Test(){
         this.setSize(1100,700);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setLayout(new BorderLayout());
 
-        panelLeft = new JPanel();
-        panelLeft.setPreferredSize(new Dimension(170,0));
+        panelLeft = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                Graphics2D g2d = (Graphics2D) g;
+                GradientPaint gradient = new GradientPaint(
+                    0, 0, new Color(0,171,255),
+                    0, getHeight(), new Color(100,100,100)
+                );
+
+                g2d.setPaint(gradient);
+                g2d.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+        panelLeft.setPreferredSize(new Dimension(190,0));
         panelLeft.setBackground(new Color(7, 140, 217));
         panelLeft.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
-
-        // label1 = new JLabel("Home");
-        // label1.setBackground(Color.cyan);
-        // label1.setPreferredSize(new Dimension(250,30));
-        // label1.setHorizontalAlignment(JLabel.CENTER);
-        // label1.setOpaque(true);
-        // label1.addMouseListener(this);
-
-        // label2 = new JLabel("About us");
-        // label2.setBackground(Color.cyan);
-        // label2.setPreferredSize(new Dimension(250,30));
-        // label2.setHorizontalAlignment(JLabel.CENTER);
-        // label2.setOpaque(true);
-
-        // label3 = new JLabel("Contact");
-        // label3.setBackground(Color.cyan);
-        // label3.setPreferredSize(new Dimension(250,30));
-        // label3.setHorizontalAlignment(JLabel.CENTER);
-        // label3.setOpaque(true);
-
         for(int i=0;i<5;i++){
+            ImageIcon icon = new ImageIcon("res/img/"+img[i]);
+            Image img = icon.getImage();
+            Image newImg = img.getScaledInstance(40,40,java.awt.Image.SCALE_SMOOTH);
+            ImageIcon newIcon = new ImageIcon(newImg);
+
             labels[i] = new JLabel(str[i]);
-            labels[i].setBackground(new Color(7, 140, 217));
+            labels[i].setOpaque(false);
+            labels[i].setBorder(null);
             labels[i].setForeground(Color.white);
-            labels[i].setPreferredSize(new Dimension(170,40));
-            labels[i].setHorizontalAlignment(JLabel.CENTER);
+            labels[i].setPreferredSize(new Dimension(190,40));
             labels[i].setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
             labels[i].setFont(new Font("Monospace",Font.PLAIN,15));
-            labels[i].setOpaque(true);
             labels[i].addMouseListener(this);
             panelLeft.add(labels[i]);
+
+            labels[i].setIcon(newIcon);
         }
         panelRight = new JPanel();
         panelRight.setBackground(Color.white);
@@ -70,15 +67,28 @@ public class Test extends JFrame implements MouseListener{
         ImageIcon newIcon = new ImageIcon(newImg);
         labelIcon1 = new JLabel();
         labelIcon1.setIcon(newIcon);
+        labelIcon1.setBorder(BorderFactory.createEmptyBorder(0,20,0,0));
 
-        panelTop = new JPanel();
+        panelTop = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                
+                Graphics2D g2d = (Graphics2D) g;
+                GradientPaint gradient = new GradientPaint(
+                    0, 0, new Color(102,0,255),
+                    0, getHeight(), new Color(0,171,255)
+                );
+                g2d.setPaint(gradient);
+                g2d.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
         panelTop.setLayout(new BorderLayout());
-        panelTop.setBackground(new Color(7, 140, 217));
         panelTop.setPreferredSize(new Dimension(0,130));
 
         panelUser = new JPanel();
         panelUser.setPreferredSize(new Dimension(250,170));
-        panelUser.setBackground(new Color(7, 140, 217));
+        panelUser.setOpaque(false);
 
         ImageIcon icon2 = new ImageIcon("res/img/user.png");
         Image img2 = icon2.getImage();
@@ -93,6 +103,9 @@ public class Test extends JFrame implements MouseListener{
         labelUserName.setForeground(Color.WHITE);
         labelUserName.setVerticalAlignment(JLabel.CENTER);
 
+        labelTitle = new JLabel("Phần mềm quản lý kho");
+        labelTitle.setHorizontalAlignment(JLabel.CENTER);
+        labelTitle.setFont(new Font("Monospace",Font.BOLD,25));
         // panelUser.setLayout(new FlowLayout(FlowLayout.CENTER,15,0));
 
         panelUser.add(labelIcon2);
@@ -100,6 +113,8 @@ public class Test extends JFrame implements MouseListener{
 
         panelTop.add(labelIcon1,BorderLayout.WEST);
         panelTop.add(panelUser,BorderLayout.EAST);
+        panelTop.add(labelTitle,BorderLayout.CENTER);
+        
 
         this.add(panelLeft,BorderLayout.WEST);
         this.add(panelRight);
@@ -134,18 +149,18 @@ public class Test extends JFrame implements MouseListener{
     public void mouseEntered(MouseEvent e) {
         for(int i=0;i<str.length;i++){
             if(e.getSource()==labels[i]){
-                labels[i].setBackground(Color.red);
+                labels[i].setBackground(new Color(223,18,133));
                 labels[i].setForeground(Color.white);
+                labels[i].setOpaque(true);
             }
         }
-
     }
     @Override
     public void mouseExited(MouseEvent e) {
         for(int i=0;i<str.length;i++){
             if(e.getSource()==labels[i]){
-                labels[i].setBackground(new Color(7, 140, 217));
-                labels[i].setForeground(Color.white);
+                labels[i].setOpaque(false);
+                labels[i].setBackground(null);
             }
         }
     }
