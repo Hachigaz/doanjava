1ad9680cd3a6d59ca167dabf43ebb3da29d96961
the bad guy <228.quochuy@gmail.com>
2023-04-04 19:44:32 +0700

    123

--- Program.java
+++ Program.java
@@ -20,31 +20,5 @@ public class Program {
     
     public void dangNhap(){
         DangNhap dn = new DangNhap(master);
-        // DangNhapUI dangnhapui = new DangNhapUI();
-        // uiHienTai = dangnhapui;
-        // ActionListener submitDanhMucSP = new ActionListener() {
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {
-        //         if(!testgiaodien){
-        //             String tentk = dangnhapui.getUsernameInput();
-        //             String mk = dangnhapui.getPasswordInput();
-                    
-        //             String sql = "select * from taikhoan_nhanvien tknv where tknv.TenTaiKhoan = '"+tentk+"' and '"+mk+"'=tknv.MatKhau";
-    
-        //             DataSet ds = master.getDataQuery(sql);
-        //             if(ds!=null){//là tìm thấy tài khoản trong csdl
-        //                 Test test = new Test(master);
-        //             }
-        //             else{//là không tìm thấy tài khoản trong csdl
-        //                 JDialog dialog = new JDialog(uiHienTai,Dialog.ModalityType.DOCUMENT_MODAL);
-        //                 dialog.setVisible(true);
-        //             }
-        //         }
-        //         else{
-        //             Test test = new Test(master);
-        //         }
-        //     }
-        // };
-        // dangnhapui.setSubmitAction(submitDanhMucSP);
     }
 }
