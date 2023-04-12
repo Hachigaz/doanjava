5fa82349324c89dabbbddc0060065ab2da22d46c
lenguyenthehien2205 <lenguyenthehien2205@gmail.com>
2023-04-12 18:59:13 +0700

    1

--- Panel/UI.java
+++ Panel/UI.java
@@ -174,7 +174,7 @@ public class UI extends JFrame implements MouseListener{
         themQuyen(new JLabel(str[0]), "res/img/"+img[0], new ThongTinSP(ds));
         themQuyen(new JLabel(str[1]), "res/img/"+img[1], new NhaCungCap(ds));
         themQuyen(new JLabel(str[3]), "res/img/"+img[3], new TaoDonNhap(ds));
-        themQuyen(new JLabel(str[4]), "res/img/"+img[4], new NhanVien(ds));
+        themQuyen(new JLabel(str[4]), "res/img/"+img[4], new NhanVien(master,tenTKDangNhap));
         TraCuuHangCTR cnTraCuuHang = new TraCuuHangCTR(master, tkDangNhap);
         themQuyen(new JLabel("Hàng trong kho"),"res/img/danhSach.png", cnTraCuuHang.getUI());
     }
